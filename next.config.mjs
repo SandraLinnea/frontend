77f8ad177da5b556e4789711d40c8230c34c2ec3
Sandra Linnea <sandra.b93@hotmail.com>
2025-10-29 09:10:12 +0100

    fixed for connecting with backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
+const API_BASE = process.env.API_BASE || "http://localhost:3002";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3002/:path*",
-      },
-    ];
+    return [{ source: "/api/:path*", destination: `${API_BASE}/:path*` }];
   },
 };
-
 export default nextConfig;
