48b4fc11abf659fdbb5df4c3b2b1e891790d7bfe
Sandra Linnea <sandra.b93@hotmail.com>
2025-10-29 10:43:35 +0100

    correct types

--- app/types/property.tsx
+++ app/types/property.tsx
@@ -1,21 +1,3 @@
-/* export type NewProperty = {
-  property_id?: string;
-  title: string;
-  description?: string;
-  city?: string;
-  country?: string;
-  price_per_night: number;
-  availability?: boolean;
-};
-
-export type Property = NewProperty & {
-  id?: string;
-  property_code?: string;
-  created_at?: string;
-  updated_at?: string;
-};
- */
-
 export type NewProperty = {
   title: string;
   description?: string;
--- app/types/user.tsx
+++ app/types/user.tsx
@@ -1,17 +1,3 @@
-/* export type NewUser = {
-  user_id?: string;
-  name: string;
-  email: string;
-  password: string;
-  is_admin?: boolean;
-};
-
-export type User = NewUser & {
-  user_id: string;
-  password_hash: string;
-  created_at: string;
-}; */
-
 export type NewUser = {
   name: string;
   email: string;
