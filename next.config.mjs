a8fa5d7ce2dad9df119aa986d49707cbf05183ad
Sandra Linnea <sandra.b93@hotmail.com>
2025-10-29 10:16:09 +0100

    change order

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-const API_BASE = process.env.API_BASE || "http://localhost:3002";
 /** @type {import('next').NextConfig} */
+const API_BASE = process.env.API_BASE || "http://localhost:3002";
 const nextConfig = {
   async rewrites() {
     return [{ source: "/api/:path*", destination: `${API_BASE}/:path*` }];
