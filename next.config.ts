ac9d135dc05edde0780027240cf97d9ee7708bba
Sandra Linnea <sandra.b93@hotmail.com>
2025-11-03 15:37:44 +0100

    try redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+/* import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+};
+
+export default nextConfig; */
+
+import type { NextConfig } from "next";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:3002";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_BASE}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
