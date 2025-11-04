b33b36536267d054c13b85864a5fbb3b92378a88
Sandra Linnea <sandra.b93@hotmail.com>
2025-11-04 09:49:54 +0100

    back to original

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-/* import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig; */
-
-import type { NextConfig } from "next";
-
-console.log("[build] API_BASE =", process.env.API_BASE);
-
-const nextConfig: NextConfig = {};
 export default nextConfig;
